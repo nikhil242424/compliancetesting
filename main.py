c2ecae1a1c7b183e92414e9cb97a66a440a95f91
nikhilkhair <khairnikhil@gmail.com>
2022-10-06 03:04:41 +0530

    code changed

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import boto3
-import pandas as pd
-import s3fs
-
-client = boto3.client('s3')
-path = 's3://cricketdataa/ODI data.csv'
-df = pd.read_csv(path)
-print(pd.options.display.max_rows)
-#print(df.info())
-#print(df.head())
-hsgt80 = df[df['50'] > '40']
-print(hsgt80)
+a=2
+b=3
+c=a+b
+print(c)
